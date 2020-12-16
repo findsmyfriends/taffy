4b6c2eb0173db9e66d06fd39d19ef6e374014a3b
taffy63 <kriengkrai.born@gmail.com>
2020-12-16 08:37:03 +0700

    Text

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,7 +29,10 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'taffy.apps.TaffyConfig',
+    'taffy',
 ]
 
 MIDDLEWARE = [
--- mysite/taffy/models.py
+++ mysite/taffy/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    # id_ = models.AutoField(primary_key=True)
+    # user_id = models.AutoField(unique=True)
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
+    # birth = models.DateField(auto_now=True)
+    # discription = models.CharField(max_length=1000,default="")
+    # sexual = models.CharField(max_length=100)
+    # sextest = models.CharField(max_length=100)
+    # link_pic = models.TextField(blank=True ,default="")
+    # age = models.IntegerField()
+    # created_at = models.DateTimeField(auto_now_add=True) # When it was create
+    # updated_at = models.DateTimeField(auto_now=True) # When i was update
+    
+    # class Meta:
+    #     ordering = ['created_at']
