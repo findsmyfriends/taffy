60eea1469a01ae8a1de94d2cedb360fce0512c43
taffy63 <kriengkrai.born@gmail.com>
2020-12-16 17:31:45 +0700

    dbml

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'taffy',
+    'django_dbml',
 ]
 
 MIDDLEWARE = [
--- mysite/taffy/serializers.py
+++ mysite/taffy/serializers.py
@@ -4,8 +4,8 @@ from .models import Member
 class MemberSerializer(serializers.HyperlinkedModelSerializer):
     class  Meta:
         model = Member
-        fields = ['first_name','last_name']
-        # fields = ['first_name','last_name','birth','discription','sexual','sextest','link_pic','age']
+       
+        fields = ['first_name','last_name','birth','discription','sexual','sextest','link_pic','age']
     
 # class UserSerializer(serializers.HyperlinkedModelSerializer):
 #     class Meta:
