46d7dd05a7865f2e85c725b3d68e1343b6d04862
taffy63 <kriengkrai.born@gmail.com>
2021-03-04 01:43:06 +0700

    del

--- TestWeb/web/taffy/admin.py
+++
@@ -1,9 +0,0 @@
-from .models import Match, Message, Profile, Rating
-
-from django.contrib import admin
-
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Rating)
-admin.site.register(Match)
-admin.site.register(Message)
