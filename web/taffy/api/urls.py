870f48cb4a1831e4d7b1a61f11fe6adde4b59d65
taffy63 <kriengkrai.born@gmail.com>
2021-05-22 16:48:07 +0700

    “Updated_to_done_and_BackUp”

--- web/taffy/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import LikeToggleAPIView
-urlpatterns = [
-    path('<int:pk>/like/', LikeToggleAPIView.as_view(), name='like_api'),
-    # path('<int:pk>/nope/', NopeToggleAPIView.as_view(), name='nope_api')
-]
